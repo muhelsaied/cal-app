e03e2c3c5a494609952faa0960289ad3a61b4a8a
muhammad elsaied <mohsfathy@outlook.com>
2020-06-07 15:18:53 +0200

    finish app

--- cal-app/Views/TxtField.swift
+++ cal-app/Views/TxtField.swift
@@ -14,6 +14,22 @@ class TxtField: UITextField {
     override func prepareForInterfaceBuilder() {
         CustizedView()
     }
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 25
+        let currencyLbl = UILabel(frame: CGRect(x:8 ,y:((frame.size.height/2) - size / 2),width:size,height:size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.5667886734, green: 0.227562964, blue: 0.9016211629, alpha: 0.7927486795)
+        currencyLbl.layer.cornerRadius = 2.0
+        currencyLbl.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text =
+            formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,6 +42,7 @@ class TxtField: UITextField {
     func CustizedView(){
         backgroundColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 0.2487345951)
         layer.cornerRadius = 5.0
+        clipsToBounds = true
         textAlignment = .center
         if let p = placeholder {
             let place = NSAttributedString(string: p,attributes: [.foregroundColor:#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
